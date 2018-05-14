ccdfb92de18ece6e6d54f9e70f7dcd1ce0e29c51
phantom05 <monster2jy@gmail.com>
2018-05-14 19:28:39 +0900

    javascript url update

---
+++ jQuery/javascript-related.js
@@ -0,0 +1,17 @@
+/* javascript related
+
+  <!-- jQuery lib -->
+
+  <!-- jQuery Core 3.3.1 -->
+
+  <script
+  src="https://code.jquery.com/jquery-3.3.1.slim.js"
+  integrity="sha256-fNXJFIlca05BIO2Y5zh1xrShK3ME+/lYZ0j+ChxX2DA="
+  crossorigin="anonymous"></script>
+
+  <!-- jQuery min.js -->
+
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+
+  */
