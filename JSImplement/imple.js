1468cdb7a0e60888c958f1387be8e406cba90a22
phantom05 <monster2jy@gmail.com>
2018-05-22 05:35:46 +0900

    feat: imple.js to includes Add modal platform

--- JSImplement/imple.js
+++ JSImplement/imple.js
@@ -35,3 +35,60 @@ file.onchange = function () {
     }
   };
 };
+
+
+
+// modal platform START
+
+/*
+<!-- madal flatt form start -->
+<!-- make use button and class="modalBt" and button data-name="" == modal data-name=="" -->
+  <!-- Trigger/Open The Modal -->
+  <button id="myBtn" class="modalBt" data-name="info">Open Modal</button>
+  <!-- The Modal / background-->
+  <div id="myModal" class="modal" data-name="info">
+    <!-- Modal content -->
+    <div class="modal-content">
+      <div class="modal-header">
+        <span class="modal__close">X</span>
+        Information
+      </div>
+      <div class="modal-body">
+        I'm Information
+      </div>
+      <!-- <div class="modal-footer"></div> -->
+    </div>
+  </div>
+  <!-- madal flatt form end -->
+
+*/
+
+let modalPlatForm = function(){
+  let modalAmount = document.getElementsByClassName('modalBt');
+  let modal =document.getElementsByClassName('modal');
+  let modalClost = document.getElementsByClassName('modal__close');
+  
+  for(var i =0;i<modalAmount.length;i++){
+   modalAmount[i].addEventListener('click',function(){
+     for(var j=0;j<modal.length;j++){
+      if(modal[j].getAttribute('data-name') == this.getAttribute('data-name')){
+       let modalTarget = modal[j];
+       let modalClose = modalClost[j];
+       modalTarget.style.display='block';
+  
+       modalClose.addEventListener('click',function(){
+        modalTarget.style.display='none';
+       })
+       window.addEventListener('click',function(e){
+         if(e.target == modalTarget){
+          modalTarget.style.display='none';
+         }
+       })
+      }
+     }
+   })
+  }
+}
+modalPlatForm()
+
+// modal platform END
