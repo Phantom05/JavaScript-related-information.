26754c2a59392c2ed8fd6d23fa2f249c02647f16
Phantom05 <monster2jy@gmail.com>
2019-05-20 07:58:04 +0900

    UPDATE : Functional Programming update

--- Functional-Programming/test.js
+++ Functional-Programming/test.js
@@ -216,19 +216,19 @@ console.log(ages);
 
 
 // 2._filter
-function _filter(users, predi) {
-  //필터는 응용형 함수, 원하는 시점에 조절할 수 있는 함수
-  var new_list = [];
-  for (var i = 0; i < users.length; i++) {
+// function _filter(users, predi) {
+//   //필터는 응용형 함수, 원하는 시점에 조절할 수 있는 함수
+//   var new_list = [];
+//   for (var i = 0; i < users.length; i++) {
 
-    if (predi(users[i])) {
-      new_list.push(users[i]);
-    }
+//     if (predi(users[i])) {
+//       new_list.push(users[i]);
+//     }
 
-  }
-  return new_list;
-  //콘솔로 소통하지 않고 리턴으로 소통하는게 좋다.
-}
+//   }
+//   return new_list;
+//   //콘솔로 소통하지 않고 리턴으로 소통하는게 좋다.
+// }
 // console.log(
 //   _filter(users, function (user) { return user.age >= 30;})
 // )
@@ -236,13 +236,13 @@ function _filter(users, predi) {
 console.log('map')
 // 3. map
 //다형성이 높고 데이터를 함수에서 전혀 찾아볼 수 없게됨. 완전히 분리가됨
-function _map(list,mapper) {
-  var new_list = [];
-  for (var i = 0; i < list.length; i++) {
-    new_list.push(mapper(list[i]))
-  }
-  return new_list
-}
+// function _map(list,mapper) {
+//   var new_list = [];
+//   for (var i = 0; i < list.length; i++) {
+//     new_list.push(mapper(list[i]))
+//   }
+//   return new_list
+// }
 
 var over_30 = _filter(users,function(user){return user.age >= 30});
 console.log(over_30);
@@ -258,6 +258,36 @@ var ages = _map(under_30,function(user){
 })
 console.log(ages);
 
+console.log(
+  _map([1,2,3],function(num){return num*2})
+);
+
+
+
+
+
+// 컬렉션 중심 프로그래밍의 4가지 유형과 함수
+
+// 1. 수집하기 - map, values, pluck 등
+// 2. 거르기 - filter, reject, compack, widthout 등
+// 3. 찾아내기 - find, some, every 등
+// 4. 접기 = reduce, min, max, group_by, count_by
+
+
+
+
+
+// let inputElm = _filter(Array.from(document.querySelectorAll(`#testForm *`)),function(elm){
+//   return elm.nodeName == 'INPUT'
+// });
+// console.log(
+//   inputElm[0].type = 'password'
+// )
+
+
+// 컬렉션
+
+
 
 // console.log(
 //   _filter(users, function (user) {return user.age < 30; })
@@ -269,3 +299,94 @@ console.log(ages);
 //   _filter([1, 2, 3, 4], function (num) { return num % 2 }),
 //   _filter([1, 2, 3, 4], function (num) { return !(num % 2) })
 // );
+
+
+
+
+console.log(
+  [1,2,3].map(function(val){ return val* val})
+);
+console.log(
+  [1,2,3].filter(function(val){return val % 2})
+);
+// 기존의 map 과 filter는 메서드임. 순수함수가 아님.
+// 또한 객체 지향 프로그래밍임.
+
+//객체지향은 반드시 객체가 생겨야 메서드를 사용 가능하기 때문에, 평가 시점이 중요하다
+try{
+  console.log(
+    document.querySelectorAll('*').map(function(node){
+      return node.nodeName;
+    })
+  );
+}catch(e){
+  console.log(e)
+}
+
+// 반면 함수형 코드의 경우 함수가 이미 존재하기 때문에 평가시점이 훨씬 유연하게 된다.
+console.log(
+  _map(document.querySelectorAll('body'),function(node){ return node.nodeName})
+);
+// 다형성 면에서 좋다.
+
+
+// 3. 내부 다형성
+// 1. predi, iter, mapper
+_map([1,2,3,4],function(v){ //두번째 인자는 보조함수, 내부함수의 다형성이 중요, 내부 다형성.
+  return v+20
+})
+// 보통 두번째 인자로 함수가 들어가면 콜백함수라고 부르는 경우가 많은데./
+// 함수의 역할의 이름을 따로 불러중요 하는것이 중요하다. 맵퍼이든지 이터이든지 프리디이든지.
+
+
+// 커링 -> 원하는 인자의 조건이 채워졌을때 본체를 실행
+
+
+
+var add = _curry(function(a ,b ){
+  return a+b;
+});
+
+var add10 = add(10);
+console.log(add10(5));
+console.log(add(5)(10));
+
+var add5 = add(5);
+console.log(add5(50));
+// 원하는 시점까지 미뤄뒀다가 원하는 시점에서 실행하는 기법. 함수형 프로그래밍은 ㄹㅇㄹㅇ이렇게 조합해 나가는 것임.
+
+console.log(add(10,20))
+
+
+var sub = _curryr(function(a ,b ){
+  return a-b;
+});
+
+console.log(sub(10,5));
+var sub5 = sub(5);
+console.log(sub5(10));// 이경우 10에서 5를 빼야하는데 표현이 조금 이상함. 위의 커리함수의경우 왼쪽에서부터 적용해나가는데 이럴때 오른쪽에서 적용하는 curryr 이라는 함수로 적용하는게 좋다.
+
+//2. _get만들어 좀 더 간단하게 하기
+
+var user1 = users[0];
+// console.log(user1.name); // 만약 user1이 없다면 에러가나게된다
+console.log(_get(user1,'name')); // _get을 사용하게되면 undefinded을 반환하게 된다.
+console.log(_get('name')(user1));
+var get_name = _get('name');
+console.log(get_name(user1));
+console.log(get_name(users[3]));
+
+
+console.log(
+  _map(
+    _filter(users,function(user){return user.age >= 30}),
+    _get('name')
+  )
+);
+
+console.log(
+  _map(
+    _filter(users,function(user){return user.age <30}),
+    _get('age')
+  )
+);
