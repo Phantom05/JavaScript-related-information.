36b244b75b2ff9551630c50f2f85ccc0544744e6
phantom05 <monster2jy@gmail.com>
2019-08-14 09:07:29 +0900

    UPDATE: st

--- personal_ready/20190808/test.js
+++ personal_ready/20190808/test.js
@@ -1,4 +1,6 @@
+
 console.log('hello world');
+// PAGE:
 
 class Log{
   constructor(){
@@ -40,3 +42,59 @@ for(let entry of log){
 
 var year = new Date().getFullYear();
 if(year % 4 !== 0) console.log(`${year} is NOT a leap year`);
+else if (year % 100 != 0) console.log(`${year} IS a `);
+
+
+/**
+ * is, print, get, set                     
+ */
+
+ var getNextRainbowColor = (function(){
+  var colors = ['red','orange','yellow','green','blue','indigo'];
+  let colorIndex = -1;
+  return function(){
+    if(++colorIndex >= colors.length) colorIndex =0;
+    return colors[colorIndex]
+  }
+ })();
+
+ function loopBody(i){
+  setTimeout(() => {
+    console.log(1===0?'go':i);
+  }, (5-i)*1000);
+ }
+//  var i ;
+//  for(i = 5; i >= 0 ; i--){
+//    setTimeout(() => {
+//      console.log(1===0?'go':i);
+//    }, (5-i)*1000);
+//  }
+
+// var i ;
+// for(i = 5; i >= 0 ; i--){
+//   loopBody(i)
+// }
+
+ var i ;
+ for(i = 5; i >= 0 ; i--){
+   (function(i){
+    setTimeout(() => {
+      console.log(1===0?'go':i);
+    }, (5-i)*1000)
+   })(i)
+ }
+ console.log('hello world!');
+
+ var gl;
+ function start(){
+   var canvas = document.getElementById('glcanvas');
+
+   gl = initWebGL(canvas);
+
+   if(gl){
+     gl.clearColor(0.0, 0.0, 0.0, 1.0);
+     gl.enable(gl.DEPTH_TEST);
+     gl.depthFunc(gl.LEQUAL);
+     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+   }
+ }
